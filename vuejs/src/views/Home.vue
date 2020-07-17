1e6bb5b917c642a7a94f571812db18bcf6255f1c
euberdeveloper <euberdeveloper@gmail.com>
2020-07-17 18:09:29 +0200

    improvement: ui

--- vuejs/src/views/Home.vue
+++ vuejs/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home grid">
     <div class="site-title">Home</div>
-    <div class="site-description">Use the above navbar to choose if you want to submit an image or a video.</div>
+    <div class="site-description">Use the above navbar to choose if you want to submit an image or a video. After that, click on the button to properly select a file. You will se a preview of the file. Click again the button to upload the file to the server and wait (it could take a lot) until the server will respound and the analyzed file downloaded.</div>
   </div>
 </template>
 
@@ -14,6 +14,8 @@ export default class Home extends Vue {}
 
 <style lang="scss" scoped>
 $gap-title: 36px;
+$width-description: 50%;
+$color-description: #494949; 
 
 .home {
   height: 100%;
@@ -26,6 +28,8 @@ $gap-title: 36px;
 
   .site-description {
     margin-top: $gap-title;
+    width: $width-description;
+    color: $color-description;
   }
 }
 </style>
