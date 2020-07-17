33ec8178e5c515b4c5b5cfe2522370c28a04f9ef
euberdeveloper <euberdeveloper@gmail.com>
2020-07-17 17:50:52 +0200

    upload

---
+++ vuejs/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home grid">
+    <div class="site-title">Home</div>
+    <div class="site-description">Use the above navbar to choose if you want to submit an image or a video.</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+$gap-title: 36px;
+
+.home {
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .site-description {
+    margin-top: $gap-title;
+  }
+}
+</style>
